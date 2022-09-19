6c8f79f18816319f0ba8b5bef60a7666aad8b8d1
ahas <dev@ahas.io>
2022-09-20 00:07:46 +0900

    Refactor: add comments and modify syntax style

--- commands/play.js
+++ commands/play.js
@@ -3,25 +3,24 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { resolve } = require("path");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("play").setDescription("play the sound"),
+  data: new SlashCommandBuilder().setName("play").setDescription("음성을 재생합니다."),
   run: async (interaction) => {
     if (!interaction.member.voice.channelId) {
-      return await interaction.reply({ content: "통화방에 들어가 있지 않습니다.", ephemeral: true });
+      return await interaction.reply({ content: "음성 채널에 들어가있지 않습니다.", ephemeral: true });
     }
 
-    const connection = joinVoiceChannel({
+    const player = createAudioPlayer();
+
+    joinVoiceChannel({
       channelId: interaction.member.voice.channelId,
       guildId: interaction.guildId,
       adapterCreator: interaction.guild.voiceAdapterCreator,
-    });
+    }).subscribe(player);
 
     // 파일명만
     const file = "nice.mp3";
-    const player = createAudioPlayer();
     const resource = createAudioResource(resolve(__dirname, `../mp3/${file}`));
-    console.log("play", file);
-
-    connection.subscribe(player);
+    console.log("재생", file);
 
     player.play(resource);
 
--- index.js
+++ index.js
@@ -1,43 +1,76 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const { Client, Collection, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+const fs = require("fs");
+const { join } = require("path");
+const { Client, Routes, Collection, GatewayIntentBits } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { clientId, guildId, token } = require("./config.json");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+const rest = new REST({ version: "10" }).setToken(token);
 
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+function initClient() {
+  // ! 중요
+  // ! GatewayIntentBits.GuildVoiceStates를 설정해줘야 음성 재생이 가능함
+  const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  client.commands.set(command.data.name, command);
-  console.log("set command: %s", command.data.name);
+  client.once("ready", () => {
+    console.log("봇 준비 완료 !");
+  });
+
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isChatInputCommand()) {
+      return;
+    }
+
+    const command = interaction.client.commands.get(interaction.commandName);
+    console.log("상호작용 생성: %s", interaction.commandName);
+
+    if (!command) {
+      return;
+    }
+
+    try {
+      await command.run(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: "명령 실행 중 오류가 발생하였습니다.", ephemeral: true });
+    }
+  });
+
+  return client;
 }
 
-client.once("ready", () => {
-  console.log("Ready!");
-});
+function initCommands(client) {
+  const commands = [];
+  const commandsPath = join(__dirname, "commands");
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+  client.commands = new Collection();
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isChatInputCommand()) {
-    return;
+  for (const file of commandFiles) {
+    const filePath = join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
+    console.log("커맨드 등록: %s", command.data.name);
   }
 
-  const command = interaction.client.commands.get(interaction.commandName);
-  console.log("interactionCreate: %s", interaction.commandName);
+  return commands;
+}
 
-  if (!command) {
-    return;
-  }
-
+async function deployCommands(commands) {
   try {
-    await command.run(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: "이 명령을 실행하는 동안 오류가 발생했습니다!", ephemeral: true });
-  }
-});
+    await rest.put(Routes.applicationCommands(clientId), { body: [] });
+    console.log("모든 커맨드가 제거되었습니다.");
 
-client.login(token);
+    const data = await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+    console.log(`${data.length}개의 애플리케이션 커맨드가 생성되었습니다.`);
+    console.log(commands.map((x, i) => `${i + 1}. ${x.name} - ${x.description}`).join("\n"));
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+(async () => {
+  const client = initClient();
+  const commands = initCommands(client);
+  await deployCommands(commands);
+  await client.login(token);
+})();
